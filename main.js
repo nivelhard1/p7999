9e7efa770b51aca9c8bc665117667f9a500ebfa9
nivelhard1 <156718101+nivelhard1@users.noreply.github.com>
2024-01-19 18:11:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+menuListArray = ["Pizza Vegetariana",//adicionar mais itens
+                    "mexica",
+                "calabresa",
+            "quatro queijos",
+        "portuquesa"];
+
+
+
+function getMenu(){
+var htmldata="";
+menuListArray.sort();
+for(var i=0;i<menuListArray.length;i++){
+htmldata=htmldata+ menuListArray[i] + '<br>'
+}
+document.getElementById("displayMenu").innerHTML = htmldata
+}
+
+function addItem(){
+var htmldata;
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata=""
+for(var i=0;i<menuListArray.length;i++){
+    htmldata=htmldata+imgtags+ menuListArray[i]+'<br>'
+}
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
+}
+
+function addTop(){
+var item=document.getElementById("AddItem").value;
+menuListArray.push(item);
+addItem();
+}
